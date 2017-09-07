19d70fdb3bb388d30b6cd7f92910737590e31328
xutongbao <xutongbao>
2017-09-07 18:47:03 +0800

    使用HtmlWebpackPlugin插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const webpack= require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     devtool: 'eval-source-map',
     entry: __dirname + "/app/main.js",   //已多次提及的唯一入口文件
@@ -35,5 +38,11 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.BannerPlugin('版权所有，翻版必究'),
+        new HtmlWebpackPlugin({
+            template: __dirname + "/app/index.tmpl.html" //new 一个这个插件的实例，并传入相关的参数
+        })
+    ]
 };
