7168a0cf7e708b9041dc86ea3f7123975f5d2725
xutongbao <xutongbao>
2017-09-07 17:54:50 +0800

    css loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,13 +16,21 @@ module.exports = {
                 test: /(\.jsx|\.js)$/,
                 use: {
                     loader: "babel-loader"
-                    // options: {
-                    //     presets: [
-                    //         "es2015", "react"
-                    //     ]
-                    // }
                 },
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    }, {
+                        loader: "css-loader",
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
             }
         ]
     }
